99b6fda503ac6c95bad12ce7b6b3dae75ca15a62
Renzo DEGIOVANNI <renzo.degiovanni@list.lu>
2024-09-24 10:03:31 +0200

    unused iports removed

--- LLMsForEduQG/MultipleChoiceQuestion.py
+++ LLMsForEduQG/MultipleChoiceQuestion.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel, Field, validator
-from typing import Optional, Union, Required
+from pydantic import BaseModel, Field
+from typing import Optional
 
 
 class MultipleChoiceQuestion(BaseModel):
